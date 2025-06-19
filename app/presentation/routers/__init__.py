b2ab1fa031055f1a5246dc2d681b4c3c5a1786de
Đinh Quốc Tuấn <dqtuanhcmus@gmail.com>
2025-06-19 07:23:38 +0700

    Update __init__.py

--- app/presentation/routers/__init__.py
+++ app/presentation/routers/__init__.py
@@ -10,7 +10,8 @@ from .auth              import router as auth         # noqa: F401
 from .exercise          import router as exercise     # noqa: F401
 from .page              import router as page         # noqa: F401
 from .profile_api       import router as profile_api       # noqa: F401
-from .api_keywords      import router as api_keywords      # noqa: F401
+from .api_keywords       import router as api_keywords      # noqa: F401
+from .api_dictionary     import router as api_dictionary    # noqa: F401
 # thêm router khác nếu có…
 
 __all__ = ["home", "api_voice", "auth", "exercise" ,"page","profile_api"]
